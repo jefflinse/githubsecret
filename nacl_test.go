9f729f496be0b4ab74f176e3d4b2da62ef121da0
Jeff Linse <jeff@jefflinse.com>
2020-12-09 14:00:40 -0800

    Add UT coverage for generating the ephemeral key pair

--- nacl_test.go
+++ nacl_test.go
@@ -26,6 +26,7 @@ func TestEncrypt(t *testing.T) {
 		name           string
 		pk             string
 		secret         string
+		generateKeyFn  func(r io.Reader) (*[32]byte, *[32]byte, error)
 		expectedOutput string
 		expectedError  error
 	}{
@@ -33,6 +34,7 @@ func TestEncrypt(t *testing.T) {
 			name:           "can encode an empty secret with a valid recipient pk",
 			pk:             mockValidPublicKey,
 			secret:         "",
+			generateKeyFn:  mockGenerateKey,
 			expectedOutput: "Gj5IXxUxMQ5tEVN7a83nwiBC/e5ckiWTkPRH3G3FNBxAvNLVFmltuTJxCdc5joBu",
 			expectedError:  nil,
 		},
@@ -40,6 +42,7 @@ func TestEncrypt(t *testing.T) {
 			name:           "can encode a non-empty secret with a valid recipient pk",
 			pk:             mockValidPublicKey,
 			secret:         mockValidSecretValue,
+			generateKeyFn:  mockGenerateKey,
 			expectedOutput: "Gj5IXxUxMQ5tEVN7a83nwiBC/e5ckiWTkPRH3G3FNBzNnh9ZMlNTPy2fTY6WSlvJ3CBqQVYj2jQwqwMSTcShAiaQCIk=",
 			expectedError:  nil,
 		},
@@ -47,6 +50,7 @@ func TestEncrypt(t *testing.T) {
 			name:           "fails when the recipient pk is empty",
 			pk:             "",
 			secret:         mockValidSecretValue,
+			generateKeyFn:  mockGenerateKey,
 			expectedOutput: "",
 			expectedError:  errors.New("recipient public key has invalid length (0 bytes)"),
 		},
@@ -54,6 +58,7 @@ func TestEncrypt(t *testing.T) {
 			name:           "fails when the recipient pk is too short",
 			pk:             mockInvalidPublicKeyTooShort,
 			secret:         mockValidSecretValue,
+			generateKeyFn:  mockGenerateKey,
 			expectedOutput: "",
 			expectedError:  errors.New("recipient public key has invalid length (30 bytes)"),
 		},
@@ -61,6 +66,7 @@ func TestEncrypt(t *testing.T) {
 			name:           "fails when the recipient pk is too long",
 			pk:             mockInvalidPublicKeyTooLong,
 			secret:         mockValidSecretValue,
+			generateKeyFn:  mockGenerateKey,
 			expectedOutput: "",
 			expectedError:  errors.New("recipient public key has invalid length (34 bytes)"),
 		},
@@ -68,14 +74,25 @@ func TestEncrypt(t *testing.T) {
 			name:           "fails when the recipient pk is not valid base64",
 			pk:             mockInvalidPublicKeyNotBase64,
 			secret:         mockValidSecretValue,
+			generateKeyFn:  mockGenerateKey,
 			expectedOutput: "",
 			expectedError:  errors.New("illegal base64 data at input byte 4"),
+		},
+		{
+			name:   "fails when the ephemeral key pair cannot be generated",
+			pk:     mockValidPublicKey,
+			secret: mockValidSecretValue,
+			generateKeyFn: func(_ io.Reader) (*[32]byte, *[32]byte, error) {
+				return nil, nil, assert.AnError
+			},
+			expectedOutput: "",
+			expectedError:  assert.AnError,
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
 			origGenerateKey := generateKey
 			defer func() { generateKey = origGenerateKey }()
-			generateKey = mockGenerateKey
+			generateKey = test.generateKeyFn
 
 			output, err := Encrypt(test.pk, test.secret)
 			if test.expectedError != nil {
